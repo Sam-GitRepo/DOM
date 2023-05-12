226f7d4b8326a908785ad33a4df8e5ca92ace6f4
Sam <smarttsamy@gmail.com>
2023-05-12 21:57:13 +0530

    querySelector

--- dom.js
+++ dom.js
@@ -58,7 +58,7 @@
 // }
 
 
-// QUERY SELECTOR 
+// QUERY SELECTOR
 
 // var header = document.querySelector('#main-header');
 // header.style.borderBottom = 'solid 4px #000';
@@ -140,31 +140,63 @@
 //createElement
 
 // create a div
-var newDiv = document.createElement('div');
+// var newDiv = document.createElement('div');
 
 //add class
-newDiv.className = 'hey';
+// newDiv.className = 'hey';
 
 //add id
-newDiv.id = 'hello'; 
+// newDiv.id = 'hello';
 
 //add attribute
-newDiv.setAttribute('title', 'hello Div');
+// newDiv.setAttribute('title', 'hello Div');
 
 //create textNode
-var newDivText = document.createTextNode("Hello Bitch");
+// var newDivText = document.createTextNode("Hello Bitch");
 
 // add text to div
-newDiv.appendChild(newDivText);
+// newDiv.appendChild(newDivText);
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
-console.log(newDiv);
+// console.log(newDiv);
 
-newDiv.style.fontSize = '30px';
+// newDiv.style.fontSize = '30px';
 
-container.insertBefore(newDiv,h1);
+// container.insertBefore(newDiv,h1);
+
+// EVENTS //
+
+// var button = document.getElementById('button').addEventListener
+// ('click', buttonClick);
+
+// function buttonClick(e) {
+    // console.log('Button Clicked');
+    // document.getElementById('header-title').textContent = 'Changed';
+    // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+    // console.log(e);
+
+    // console.log(e.target);
+    // console.log(e.target.id);
+    // console.log(e.target.className);
+    // console.log(e.target.classList);
+    // var output = document.getElementById('output');
+    // output.innerHTML = '<h3>'+e.target.id+'</h3>'
+
+    // console.log(e.type);
+
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+
+    // console.log(e.offsetX);
+    // console.log(e.offsetY);
+
+    // console.log(e.altKey);
+    // console.log(e.shiftKey);
+    // console.log(e.ctrlKey);
+
+// }
 
 
 
