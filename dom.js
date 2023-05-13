40ead13804d4d55504b918476db74621deaf8ae9
Sam <smarttsamy@gmail.com>
2023-05-13 15:11:53 +0530

    DOM Tut

--- dom.js
+++ dom.js
@@ -198,8 +198,8 @@
 
 // }
 
-var button = document.getElementById('button');
-var box = document.getElementById('box');
+// var button = document.getElementById('button');
+// var box = document.getElementById('box');
 
 // button.addEventListener('click', runEvent);
 // button.addEventListener('dblclick', runEvent);
@@ -214,9 +214,9 @@ var box = document.getElementById('box');
 
 // box.addEventListener('mousemove', runEvent);
 
-var itemInput = document.querySelector('input[type="text"]');
-var form = document.querySelector('form');
-var select = document.querySelector('select');
+// var itemInput = document.querySelector('input[type="text"]');
+// var form = document.querySelector('form');
+// var select = document.querySelector('select');
 
 
 // itemInput.addEventListener('keydown',runEvent);
@@ -234,11 +234,11 @@ var select = document.querySelector('select');
 // select.addEventListener('change', runEvent);
 // select.addEventListener('input', runEvent);
 
-form.addEventListener('submit',runEvent);
+// form.addEventListener('submit',runEvent);
 
-function runEvent(e){
-    e.preventDefault();
-    console.log('EVENT TYPE: '+e.type);
+// function runEvent(e){
+//     e.preventDefault();
+//     console.log('EVENT TYPE: '+e.type);
     
     // console.log(e.target.value);
     // document.getElementById('output').innerHTML = '<h3>' +e.target.value+ '</h3>';
@@ -246,7 +246,7 @@ function runEvent(e){
     // output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
 
     // document.body.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+',40)';
-}
+// }
 
 
 
